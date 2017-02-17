7494c6ace9f22513acfde7a5099d6b76a9b79f9f
zhang <994029404@qq.com>
2017-02-17 17:41:54 +0800

    登陆完成

---
+++ Administration/Administration/Class/ZxdObject.h
@@ -0,0 +1,13 @@
+//
+//  ZxdObject.h
+//  Administration
+//
+//  Created by zhang on 2017/2/17.
+//  Copyright © 2017年 九尾狐. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZxdObject : NSObject
+
+@end
---
+++ Administration/Administration/Class/ZxdObject.m
@@ -0,0 +1,13 @@
+//
+//  ZxdObject.m
+//  Administration
+//
+//  Created by zhang on 2017/2/17.
+//  Copyright © 2017年 九尾狐. All rights reserved.
+//
+
+#import "ZxdObject.h"
+
+@implementation ZxdObject
+
+@end
